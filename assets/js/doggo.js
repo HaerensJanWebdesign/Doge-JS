ca71e33c71a29f7173229b6cfeafe6dfacbf93e5
JanHaerensBeCode <haerens.jan@gmail.com>
2019-05-27 12:00:50 +0200

    button has a hover-effect using java

--- assets/js/doggo.js
+++ assets/js/doggo.js
@@ -1,3 +1,18 @@
-window.onload = function() {
-    document.getElementById("doge").innerHTML = "WOOF!";
+function changeText(){
+
+var text = document.getElementById('doge').innerHTML = "Woof woof!"
+
 }
+
+function defaultText(){
+
+    var text = document.getElementById('doge').innerHTML = "Woof!"
+    
+    }
+
+window.onload = function() {
+
+    document.getElementById("doge").innerHTML = "WOOF!";
+
+}
+
